b84a72e06b7d10b4ba0718455241eb20280ffa36
Kirill Kuklin <kk90@mail.com>
2017-02-12 22:02:23 +0300

    working min and max function. to be fixed: max(range(6))

--- Min and Max.py
+++ Min and Max.py
@@ -10,7 +10,6 @@ def max(*args, **kwargs):
                 return n
             elif type(i) == list:
                 for m in i:
-                    # n = 0
                     if m > n:
                         n = m
                 return m
@@ -26,12 +25,16 @@ def max(*args, **kwargs):
 
 def min(*args, **kwargs):
     key = kwargs.get("key", lambda x: x)
+    # print(len(args))
     if len(args) == 1:
         args = args[0]
+        # print(args)
         low = None
         for d in args:
+            # print(key(d))
             if low == None or key(d) < key(low):
                 low = d
+                # print(low, key(d) < key(low))
         return low
     else:
         for i in args:
@@ -47,16 +50,11 @@ def min(*args, **kwargs):
                     if ord(m) < low:
                         low = ord(m)
                 return chr(low)
-            # elif type(i) == list:
-            #     low = i[0]
-            #     for d in i:
-            #         if d < low:
-            #             low = d
-            #     return low
 
 print(max(3, 2,6, 646))
 print(min(3, 2))
 print(max([1, 2, 0, 3, 4]))
 print(min("hello"))
 print(max(2.2, 5.6, 5.9, key=int))
-print(min([[1, 2], [3, 0], [9, 12]], key=lambda x: x[1]))
+print(min([[1, 1], [3, 12], [9, 0]], key=lambda x: x[1]))
+# print(2 < None)
